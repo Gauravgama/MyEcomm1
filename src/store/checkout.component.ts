139adb20689add7d6e09e325ad2218675c8813c7
Gaurav Kumar <gaurav.kumar5@in.ey.com>
2025-09-04 11:30:56 +0530

    checkout component

--- src/store/checkout.component.ts
+++ src/store/checkout.component.ts
@@ -1,6 +1,8 @@
 import { OrderRepository } from './../app/model/order.repository';
-import { Order } from './../app/model/order.model';
+import { ModelModule } from './../app/model/model.module';
+import { NgForm } from '@angular/forms';
 import { Component } from '@angular/core';
+import { Order } from '../app/model/order.model';
 
 @Component({
   selector: 'checkout',
@@ -8,5 +10,15 @@ import { Component } from '@angular/core';
   standalone: false,
 })
 export class CheckOut {
-  constructor(private repository: OrderRepository, public order: Order) {}
+  orderSent: boolean = false;
+  submitted: boolean = false;
+
+  constructor(private repository: OrderRepository, public order: Order) {
+    // order = cart service
+    //this.order - bunch of records
+  }
+
+  submitOrder(form: NgForm) {}
+
+
 }
